18f2dbefaacc1f59f220f04b0eff56ff19ba0cbe
Adelina <adelina.romascu@gmail.com>
2021-01-17 21:15:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1
+{
+    class Program
+    {  
+        //Problema 1
+        //Se da o secventa de n numere. Sa se determine cate din ele sunt pare. 
+
+        static int pare(int[] v)
+        {
+            int nrPare = 0;
+
+            for(int i = 0;i < v.Length; i++)
+            {
+                if(v[i]%2 == 0)
+                {
+                    nrPare++;
+                }
+            }
+            return nrPare;
+        }
+
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Introdu n:");
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            Console.WriteLine("Introdu numerele: ");
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("V[" + i + "]:");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(v[i] + " ");
+                 
+            }
+            Console.WriteLine();
+            Console.WriteLine("Numere pare: " + pare(v));
+        }
+    }
+}
